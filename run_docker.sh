854f10669bfb1a03eca48f530a0b7b4daadcb64c
Noam Tuchman <tuchman.bear@gmail.com>
2019-08-11 11:14:47 -0400

    I edited stuff

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=api .
+docker build --tag=site .
 
 # Step 2: 
 # List docker images
@@ -12,8 +12,5 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -it {image name} bash
+docker run -p 8000:80 site
 
-or 
-
-docker run -p 8000:5001 api
